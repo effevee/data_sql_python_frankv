712ec11d5670809a82d329c7f0b3874c7c727239
effevee <effevee@gmail.com>
2022-09-27 16:03:43 +0200

    les 4

--- dbs_en_python/write_df_in_db_table.py
+++ dbs_en_python/write_df_in_db_table.py
@@ -20,8 +20,8 @@ df=pd.read_csv('/home/frank/Documents/data_sql_python_frankv/Measurement_info.cs
                dtype={'Station code':'int8', 'Item code':'int8', 'Instrument status':'int8', \
                       'Average value':'float16','Measurement date':'string'})
     
-insert_query='''INSERT INTO air_pol_measurement(measure_time,station_id,measure_type_id,average_val,sensor_state)
-values(%s,%s,%1,%s,%s)'''
+insert_query='''INSERT INTO air_pol_measurement(measure_time,station_id,
+measure_type_id,average_val,sensor_state) values(%s,%s,%s,%s,%s)'''
 
 # connectie maken met mysql database proto1 als gebruiker dev1
 cn=sql.connect(db=db,user=user,password=passwd,host=host)
@@ -32,5 +32,12 @@ print('got cursor:',cr)
 data=list(zip(df['Measurement date'],df['Station code'],df['Item code'],
               df['Average value'],df['Instrument status']))
 
+try:
+    cr.executemany(insert_query, data)
+    cn.commit()
+except Exception as E:
+    print('problemen met schrijven naar databank')
+    print(E)
+    
 cr.close() # cursor afsluiten
 cn.disconnect()  # connectie met database afsluiten
